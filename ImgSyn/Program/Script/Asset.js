9e236598745b3b32283bcb487ae42873f21a12af
pupungeggang2 <kyongkyong2892@gmail.com>
2024-07-22 16:42:35 +0900

    0.0.0

--- ImgSyn/Program/Script/Asset.js
+++ ImgSyn/Program/Script/Asset.js
@@ -1,7 +1,15 @@
 let img = {
+    newFile : new Image(),
+    save : new Image(),
 
+    cube : new Image(),
+    glass : new Image()
 }
 
 function loadImage() {
-    
+    img.save.src = 'Image/Save.png'
+    img.newFile.src = 'Image/New.png'
+
+    img.cube.src = 'Image/Cube.png'
+    img.glass.src = 'Image/Glass.png'
 }
